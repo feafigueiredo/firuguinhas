e2bd66df2311a0c36bc46dd6d2726f0677bdefbe
Fernando Figueiredo <ferafigueiredo@gmail.com>
2016-02-02 11:29:45 -0200

    Arrumado rotas

--- firuguinhas/web/js/app/app.js
+++ firuguinhas/web/js/app/app.js
@@ -8,35 +8,35 @@ app.config(function($routeProvider, $locationProvider)
    $routeProvider
  
    // para a rota '/', carregaremos o template home.html e o controller 'HomeCtrl'
-   .when('/', {
+   .when('/#', {
       templateUrl : '/firuguinhas/web/js/app/Views/home.html',
       controller     : 'HomeCtrl',
    })
  
    // para a rota '/sobre', carregaremos o template sobre.html e o controller 'SobreCtrl'
-   .when('/about', {
+   .when('/#about', {
       templateUrl : '/firuguinhas/web/js/app/Views/about.html',
       controller  : 'AboutCtrl',
    })
  
    // para a rota '/contato', carregaremos o template contato.html e o controller 'ContatoCtrl'
-   .when('/contact', {
+   .when('/#contact', {
       templateUrl : '/firuguinhas/web/js/app/Views/contact.html',
       controller  : 'ContactCtrl',
    })
 
     // para a rota '/login', carregaremos o template contato.html e o controller 'LoginCtrl'
-   .when('/login', {
+   .when('/#login', {
       templateUrl : '/firuguinhas/web/js/app/Views/login.html',
       controller  : 'LoginCtrl',
    })
    
    // para a rota '/login', carregaremos o template contato.html e o controller 'LoginCtrl'
-   .when('/signup', {
+   .when('/#signup', {
       templateUrl : '/firuguinhas/web/js/app/Views/signup.html',
       controller  : 'SignUpCtrl',
    })
    
    // caso não seja nenhum desses, redirecione para a rota '/'
-   .otherwise ({ redirectTo: '/' });
+   .otherwise ({ redirectTo: '/#' });
 });
