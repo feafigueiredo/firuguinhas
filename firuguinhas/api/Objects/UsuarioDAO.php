85646237cdc574f21a913965abe5430be1e8e6a8
Fernando Figueiredo <ferafigueiredo@gmail.com>
2016-02-10 11:21:49 -0200

    Committing new files

---
+++ firuguinhas/api/Objects/UsuarioDAO.php
@@ -0,0 +1,71 @@
+<?php
+class UsuarioDAO{
+      
+    // database connection and table name
+    private $conn;
+    private $table_name = "Usuario";
+      
+    // object properties
+    public $user;
+    public $name;
+    public $pass;
+    public $list;
+    
+    // constructor with $db as database connection
+    public function __construct($db){
+        $this->conn = $db;
+    }
+    
+    public function create(){
+    	 
+    	// query to insert record
+    	$query = "INSERT INTO " . $this->table_name . "
+            SET
+    			user=:user, 
+    			name=:name, 
+    			pass=:pass";
+    	 
+    	// prepare query
+    	$stmt = $this->conn->prepare($query);
+    
+    	// bind values
+    	$stmt->bindParam(":user", $this->user);
+    	$stmt->bindParam(":name", $this->name);
+    	$stmt->bindParam(":pass", $this->pass);
+    	 
+    	// execute query
+    	if($stmt->execute()){
+    		return true;
+    	}else{
+    		echo "<pre>";
+    		print_r($stmt->errorInfo());
+    		echo "</pre>";
+    
+    		return false;
+    	}
+    }
+    
+    public function get(){
+    	$query = "SELECT user, name FROM " . $this->table_name;
+    	if($this->user != null){
+    		$query = $query . " WHERE user = '" . user . "'";
+    	}
+    	
+    	if(!$rs = $mysqli->query($query)){
+    		return false;
+    	}
+    	
+    	$this->list = array();
+    	while ($dados = $rs->fetch_array()) {
+    		$newUser = new UsuarioDAO($this->db);
+    		
+    		$newUser->user = $dados[user];
+    		$newUser->name = $dados[name];
+    		
+    		array_push($list, $newUser);
+    	}
+    	
+    	return true;
+    }
+}
+?>
---
+++ firuguinhas/api/Services/usuario.php
@@ -0,0 +1,45 @@
+<?php
+// get database connection
+include_once 'Config/Database.php';
+ 
+$database = new Database();
+$db = $database->getConnection();
+ 
+// instantiate product object
+include_once 'Objects/UsuarioDAO.php';
+$user = new UsuarioDAO($db);
+
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+	// get posted data
+	$data = json_decode(file_get_contents("php://input")); 
+	 
+	// set product property values
+	$product->user = $data->user;
+	$product->name = $data->name;
+	$product->pass = $data->pass;
+	     
+	// create the product
+	if($product->create()){
+	    echo "Product was created.";
+	}
+	 
+	// if unable to create the product, tell the user
+	else{
+	    echo "Unable to create product.";
+	}
+}
+
+if($_SERVER['REQUEST_METHOD'] == 'GET'){
+	$user->user = $_GET["user"];
+	$user->name = $_GET["name"];
+
+	if($user->get()){
+		echo json_encode($user->list);
+	}else{
+		echo "Error.";
+	}
+
+	
+}
+?>
