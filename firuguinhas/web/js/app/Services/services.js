6291e6daa48f7de46bc8d95414077e52c24846b8
Fernando Figueiredo <ferafigueiredo@gmail.com>
2016-02-16 12:23:12 -0200

    Added more logs

--- firuguinhas/web/js/app/Services/services.js
+++ firuguinhas/web/js/app/Services/services.js
@@ -31,6 +31,9 @@ app.factory('UsuarioService', ['$http', function($http){
 		$http.get(baseUrl, data)
     	.then(function(response) {
     		console.log("Status: " + response.status);
+    		console.log("  Data: " + response.data );
+    		console.log("Header: " + response.headers );
+    		console.log("  Text: " + response.statusText);
     		return response.data;
         });
 	};
